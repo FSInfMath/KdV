4953f9078f7095609cb3045ae1ee6aa819a3142d
fabian <froehlich.fabian@t-online.de>
2014-12-06 16:34:33 +0100

    das ist der merge

--- server.js
+++ server.js
@@ -19,14 +19,29 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/static/landing.html');
 });
 
-app.get('/admin', function (req, res) {
-    res.sendFile(__dirname + '/static/admin.html');
-});
-
-app.get('/products', function (req, res) {
 //file system: synchron alle dateien im ordner auflisten
 //arrays in javascript angucken
 // json.stringify : aus javascript objekt nen string
+app.get('/products', function (req, res) {
+    res.send(JSON.stringify(fs.readdirSync(__dirname + '/data/products').map(function(item){
+       return item.split('.')[0];
+    })));
+});
+
+app.get('/users', function (req, res) {
+    res.send(JSON.stringify(fs.readdirSync(__dirname + '/data/users').map(function(item){
+       return item.split('.')[0];
+    })));
+});
+
+app.get('/transactions', function (req, res) {
+    res.send(JSON.stringify(fs.readdirSync(__dirname + '/data/transactions').map(function(item){
+       return item.split('.')[0];
+    })));
+});
+
+app.get('/admin', function (req, res) {
+    res.sendFile(__dirname + '/static/admin.html');
 });
 
 app.get('/product=:productid', function (req, res) {
